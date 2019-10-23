8e4f0133182a8216f780b5c05c5f10f32be4d7c2
Michael Omori <omorim@hawaii.edu>
2019-10-22 14:49:55 -1000

    float

--- pytorch/main.py
+++ pytorch/main.py
@@ -85,7 +85,7 @@ def train(args, io):
             batch_size = data.size()[0]
             opt.zero_grad()
             logits = model(data)
-            loss = criterion(logits, label)
+            loss = criterion(logits, float(label))
             loss.backward()
             opt.step()
             preds = logits.max(dim=1)[1]
